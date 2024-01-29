56c7ed78d324a8875d8e7be02d758fa1ba246846
Abhinav <abhinavhooda223@gmail.com>
2024-01-29 14:18:51 +0530

    file and folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import { CssBaseline, Grid } from '@material-ui/core';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 function App() {
   return (
-   <></>
+   <>
+   <CssBaseline/>
+   <Header/>
+   <Grid container spacing={3} style={{width:'100%'}}>
+      <Grid item xs={12} md={4}>
+          <Map/>
+      </Grid>
+   </Grid>
+   </>
   );
 }
 
