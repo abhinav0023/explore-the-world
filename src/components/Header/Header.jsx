baac87acb96abd09c0c89b0e4bde94fb75baac76
Abhinav <abhinavhooda223@gmail.com>
2024-02-14 20:20:22 +0530

    completed hearder component and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from '@material-ui/icons/Search'
+import { Autocomplete } from "@material-ui/lab";
+import useStyles from './styles'
+
 
 function Header() {
+  const classes = useStyles();
+
   return (
-    <div>Header</div>
-  )
+    <>
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h5" className={classes.title}>
+            Explore-the-world
+          </Typography>
+          <Box display="flex">
+            <Typography variant="h6" className={classes.title}>
+              Expore new places
+            </Typography>
+            <Autocomplete>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}}/>
+              </div>
+            </Autocomplete>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
 
-export default Header
+export default Header;
